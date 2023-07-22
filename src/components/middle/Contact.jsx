12e29e1804877762926d0e34508283ccef4abf41
akb3y <alisha.barraw@gmail.com>
2023-07-22 11:40:06 -0600

    fixed video size

--- src/components/middle/Contact.jsx
+++ src/components/middle/Contact.jsx
@@ -3,10 +3,11 @@ import { TfiEmail } from 'react-icons/tfi';
 import { FaGithub } from 'react-icons/fa'
 
 function Contact() {
+  var contact = window.innerWidth < 1200 ? "c" : "C"
   return (
     <div>
       <h2 className='contact'>Contact Me</h2>
-      <div className="Contact">
+      <div className={contact}>
       <a href="https://www.linkedin.com/in/alisha-barraw/"><BsLinkedin />  Alisha Barraw</a> <br />
       <a href="mailto:alisha.barraw@gmail.com"><TfiEmail />  Click to Send an Email to me</a>
       <br />
--- src/components/middle/Projects/Godmode.jsx
+++ src/components/middle/Projects/Godmode.jsx
@@ -5,9 +5,11 @@ function Godmode() {
     <div className="Godmode pr">
      <div className="BM">GODMODE</div>
   <p>GODMODE is a game recomendation app that allows the user to refine results based on parameters such as number of players and minimum age of players. Filtered results are returned in order of highest rated game first.</p>
-      <video controls autoPlay loop muted>
+  <div  className='GM'>
+      <video style={{ maxWidth: '100%', height: 'auto', display: 'block', margin: '0 auto' }}>
           <source src={Video} type="video/mp4"></source>
         </video>
+  </div>
     </div>
   );
 }
